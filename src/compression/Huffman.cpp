b152ab80c8d512349739b4465c21a7ceaa29d21f
Vinicius Dantas <vdj@cin.ufpe.br>
2022-05-08 16:29:25 -0300

    compression done

--- src/compression/Huffman.cpp
+++ src/compression/Huffman.cpp
@@ -2,7 +2,9 @@
 const int ALPHABET_SIZE = 128;
 
 std::unique_ptr<Huffman::Node> Huffman::root = nullptr;
-std::unordered_map<unsigned char, std::string> Huffman::huffmanCodes = nullptr;
+std::unordered_map<unsigned char, std::string> Huffman::huffmanCodes = std::unordered_map<unsigned char, std::string>();
+size_t Huffman::curBytePos = 0;
+unsigned char Huffman::curBitPos = 0;
 
 void Huffman::Compress(const std::string &inputFile, const std::string &outputFile)
 {
@@ -32,10 +34,14 @@ void Huffman::Compress(const std::string &inputFile, const std::string &outputFi
     PrintTree(root, 0);
 
     Encode(text);
-    // std::ofstream zipStream(outputFile, std::ios::binary);
-    // zipStream.write(reinterpret_cast<const char*>(&text[0]), text.size());
 
-    // zipStream.close();   
+    // std::cout << sizeof(unsigned char) << " " << sizeof(unsigned int) << " !!!\n";
+
+    std::ofstream zipStream(outputFile, std::ios::binary);
+    zipStream.write(reinterpret_cast<const char*>(&frequency[0]), sizeof(unsigned int) * frequency.size());
+    zipStream.write(reinterpret_cast<const char*>(&curBitPos), sizeof(unsigned char));
+    zipStream.write(reinterpret_cast<const char*>(&text[0]), curBytePos);
+    zipStream.close();
 }
 
 void Huffman::GetCodes(std::unique_ptr<Huffman::Node> &cursor, std::string &codesBuffer)
@@ -47,25 +53,70 @@ void Huffman::GetCodes(std::unique_ptr<Huffman::Node> &cursor, std::string &code
     }
     for (int bit = 0; bit < 2; bit++)
     {
-        codesBuffer.emplace_back(char(bit+48));
+        codesBuffer.push_back(char(bit+48));
         GetCodes(cursor->childs[bit], codesBuffer);
         codesBuffer.pop_back();    
     }
     return;
 }
 
+void Huffman::SetBit(unsigned char &byte, unsigned char pos)
+{
+    byte |= (static_cast<unsigned char>(1) << pos);
+}
+
 void Huffman::Encode(std::vector<unsigned char> &text)
 {
     std::string codesBuffer = "";
-    huffmanCodes = std::unordered_map<unsigned char, std::string>();
     GetCodes(root, codesBuffer);
+    
+    curBytePos = 0;
+    curBitPos = 0;
+
+    std::string leftOverBits = "";
+    size_t leftOverIndex = 0;
 
     for (size_t i = 0; i < text.size(); i++)
     {
+        unsigned char curChar = text[i];
+        text[i] = 0;
+        
+        // Place possible remaining bits from previous iterations
+        if (leftOverBits.size() != leftOverIndex)
+        {
+            for (size_t j = leftOverIndex; j < leftOverBits.size(); j++)
+            {
+                if (curBytePos > i)
+                {
+                    break;
+                }
+                if (leftOverBits[j] != '0') SetBit(text[i], curBitPos);
+                curBitPos += 1;
+                curBitPos %= 8;
+                curBytePos += (curBitPos == 0);
+                leftOverIndex += 1;
+            } 
+        }
 
-    }
-
-    
+        if (curBytePos > i)
+            continue;
+        
+        const std::string &charCode = huffmanCodes.at(curChar);
+        
+        // Place current charCode
+        for (size_t j = 0; j < charCode.size(); j++)
+        {
+            if (curBytePos > i)
+            {
+                leftOverBits += charCode.substr(j);
+                break;
+            }
+            if (charCode[j] != '0') SetBit(text[i], curBitPos);
+            curBitPos += 1;
+            curBitPos %= 8;
+            curBytePos += (curBitPos == 0);
+        }
+    }    
 }
 
 std::vector<unsigned int> Huffman::GetCharFrequency(const std::vector<unsigned char> &text)
--- src/compression/Huffman.h
+++ src/compression/Huffman.h
@@ -5,6 +5,8 @@
 #include <memory>
 #include <algorithm>
 #include <unordered_map>
+#include <string>
+#include <string_view>
 
 class Huffman 
 {
@@ -22,11 +24,14 @@ public:
 private:
     static std::unique_ptr<Node> root;
     static std::unordered_map<unsigned char, std::string> huffmanCodes;
+    static size_t curBytePos;
+    static unsigned char curBitPos;
     static void PrintTree(std::unique_ptr<Node> &node, int level);
     static void BuildTree(const std::vector<unsigned int> &frequency);
     static std::vector<unsigned int> GetCharFrequency(const std::vector<unsigned char> &text);
     static void Encode(std::vector<unsigned char> &text);
     static void GetCodes(std::unique_ptr<Node> &cursor, std::string &codesBuffer);
+    static void SetBit(unsigned char &byte, unsigned char pos);
 public:
     static void Compress(const std::string &inputFile, const std::string &outputFile);
     static void Decompress(const std::string &inputFile, const std::string &outputFile);
