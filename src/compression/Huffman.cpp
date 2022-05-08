2a933d4904f64a9b02422c61e8f75ec42c2012b2
Vinícius Dantas <vdj@cin.ufpe.br>
2022-05-08 01:57:13 -0300

    adding huffman methods

--- src/compression/Huffman.cpp
+++ src/compression/Huffman.cpp
@@ -1,4 +1,5 @@
 #include <Huffman.h>
+const int ALPHABET_SIZE = 128;
 
 void Huffman::Compress(const std::string &inputFile, const std::string &outputFile)
 {
@@ -16,9 +17,47 @@ void Huffman::Compress(const std::string &inputFile, const std::string &outputFi
 
     std::vector<unsigned char> codedText;
     codedText.reserve(text.size());
+    const std::vector<unsigned int> frequency = GetCharFrequency(text);
+
+    BuildTree(frequency);
+
+
 
     // std::ofstream zipStream(outputFile, std::ios::binary);
     // zipStream.write(reinterpret_cast<const char*>(&text[0]), text.size());
 
     // zipStream.close();   
+}
+
+std::vector<unsigned int> Huffman::GetCharFrequency(const std::vector<unsigned char> &text)
+{
+    std::vector<unsigned int> frequency(ALPHABET_SIZE);
+    
+    for (auto ch : text)
+    {
+        frequency[ch]++;
+    }
+    
+    return frequency;
+}
+
+void Huffman::BuildTree(const std::vector<unsigned int> &frequency)
+{
+    std::vector<std::unique_ptr<Node>> heap;
+    heap.reserve(ALPHABET_SIZE);
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if (frequency[i] != 0)
+        {
+            heap.emplace_back(std::make_unique<Node>(static_cast<char>(i), frequency[i]));
+        }
+    }
+
+    std::make_heap(heap.begin(), heap.end());
+
+    for (auto &e : heap)
+    {
+        std::cout << e->frequency << "\n";
+    }
 }
--- src/compression/Huffman.h
+++ src/compression/Huffman.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <algorithm>
 
 class Huffman 
 {
@@ -10,15 +11,23 @@ class Huffman
     {
         char symbol;
         int frequency;
-        std::unique_ptr<Node> left;
-        std::unique_ptr<Node> right;
+        std::unique_ptr<Node> childs[2];
 
         Node();
         Node(char symbol, int frequency, std::unique_ptr<Node> left = nullptr, std::unique_ptr<Node> right = nullptr) :
-            symbol(symbol), frequency(frequency), left(std::move(left)), right(std::move(right)) {};
+            symbol(symbol), frequency(frequency), childs { std::move(left), std::move(right) } {};
     };
-    
+private:
+    static std::unique_ptr<Node> root = nullptr;
+
 public:
     static void Compress(const std::string &inputFile, const std::string &outputFile);
     static void Decompress(const std::string &inputFile, const std::string &outputFile);
+    static std::vector<unsigned int> GetCharFrequency(const std::vector<unsigned char> &text);
+    static void BuildTree(const std::vector<unsigned int> &frequency);
 };
+
+bool operator<(const std::unique_ptr<Node> &left, const std::unique_ptr<Node> &right)
+{
+    return left->frequency < right->frequency;
+}
